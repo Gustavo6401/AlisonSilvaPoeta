d07dc0897b3d5be6e9a6687cea349317465b9918
Gustavo6401 <gsoey2019123@gmail.com>
2024-02-11 21:41:13 -0300

    .

--- AlisonSilvaIdentityUser/Program.cs
+++ AlisonSilvaIdentityUser/Program.cs
@@ -58,7 +58,15 @@ builder.Services.ConfigureApplicationCookie(options =>
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+    options.UseSqlServer(connectionString, 
+    sqlServerOptionsAction: sqlOptions =>
+    {
+        sqlOptions.EnableRetryOnFailure(
+            maxRetryCount: 10,
+            maxRetryDelay: TimeSpan.FromMinutes(2),
+            errorNumbersToAdd: null
+        );
+    }));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
